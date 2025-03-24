e80a272d553a86b0663287156222567393985722
Jesus Garland <33094958+jgarlandh@users.noreply.github.com>
2025-03-24 00:55:03 +0000

    fix/flux.js

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,6 +2,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: { 
 			characters:[],
+			planets:[],
+			vehicles:[],
 			
 		},
 		actions: {
@@ -17,6 +19,30 @@ const getState = ({ getStore, getActions, setStore }) => {
 				console.log (error)
 			}	
 			},
+
+			getPlanets: async () => {
+				try {
+				const response = await fetch("https://swapi.dev/api/planets/")
+				console.log (response)	
+				const data = await response.json() 
+				console.log (data.results)
+				setStore({planets:data.results})
+				} catch (error) {
+					console.log (error)
+				}	
+				},
+
+			getVehicles: async () => {
+					try {
+					const response = await fetch("https://swapi.dev/api/vehicles/")
+					console.log (response)	
+					const data = await response.json() 
+					console.log (data.results)
+					setStore({vehicles:data.results})
+					} catch (error) {
+						console.log (error)
+					}	
+					},
 			
 		}
 	};
